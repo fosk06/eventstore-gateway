7f2ce891cf5ad5942a00050bddfba0cc06d37ffe
fosk06 <thomastrividic@gmail.com>
2021-03-30 00:16:02 +0200

    super michel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate dotenv;
+use dotenv::dotenv;
+use crate::toto::titi;
+
+#[macro_use]
+extern crate dotenv_codegen;
+
 fn main() {
     println!("Hello, world!");
+    dotenv().ok();
+    println!("{}", dotenv!("OUTPUT_STREAM"));
 }
